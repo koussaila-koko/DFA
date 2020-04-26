7f57ae9dfa6e07d31855bbc0101eb52bd8b765df
Koussaila HAMMOUCHE <Koussaila.hammouche@ens.uvsq.fr>
2020-04-26 02:07:15 +0200

    Create DES_K.h

---
+++ App/head/DES_K.h
@@ -0,0 +1,8 @@
+#ifndef H_DES_K
+#define H_DES_K
+
+// Recherche de la clé de 64 bits K du DES à partir du clair, du chiffré et de K16
+
+long rechercheK(long, long, long);
+long chercher_k(long ,long ,long );
+#endif
